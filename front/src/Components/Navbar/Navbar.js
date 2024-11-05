e40040e331648996cce299195a65bc1509a42bc7
Rockey C.R <rockeyrai234@gmail.com>
2024-11-05 19:39:10 +0545

    Ending frontEnd work

--- front/src/Components/Navbar/Navbar.js
+++ front/src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { ShopContext } from "@/Context/shopcontext";
@@ -9,13 +9,20 @@ const Navbar = () => {
 
   const [menu,setMenu]= useState('shop')
  const {getTotalCartItems} = useContext(ShopContext)
+ const menuRef = useRef()
+
+ const dropdown_toggle = (e)=>{
+  menuRef.current.classList.toggle('nav-menu-visible');
+  e.target.classList.toggle('open')
+ }
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src='./logo.png' alt="" />
         <p>Rockey</p>
       </div>
-      <ul className="nav-menu">
+      <img className="nav-dropdown" onClick={dropdown_toggle} src="/nav_dropdown.png"/>
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>  {menu==='shop'&&<hr/>}</li>
         <li onClick={()=>{setMenu("mens")}}> <Link to='/mens'>Mens</Link> {menu==='mens'&&<hr/>}</li>
         <li onClick={()=>{setMenu("womens")}}><Link to='/womens'>Womens</Link> {menu==='womens'&&<hr/>}</li>
