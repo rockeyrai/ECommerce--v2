055d5c2cb3b2128624bf68dfb9797cf0aff6e29a
Rockey C.R <rockeyrai234@gmail.com>
2024-11-17 16:21:24 +0545

    adding funtion to delete the products

--- server/index.js
+++ server/index.js
@@ -150,8 +150,18 @@ const Product = mongoose.model("Product",{
 
 //endpoint for the product
 app.post('/addproduct',async(req,res)=>{
+  let products = await Product.find({});
+  let id;
+  if(products.length>0){
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id+1
+  }else{
+    id = 1
+  }
+
   const product= new Product({
-    id:req.body.id,
+    id:id,
     name:req.body.name,
     image:req.body.image,
     category:req.body.category,
@@ -166,6 +176,17 @@ app.post('/addproduct',async(req,res)=>{
 
   })
 })
+
+//creating API for deleting Products
+app.post('/removeproduct', async (req,res)=>{
+  await Product.findOneAndDelete({id:req.body.id})
+  console.log("Removed");
+  res.json({
+    success:true,
+    namne:req.body.name
+  })
+})
+
 
 // Start the server
 app.listen(port, () => {
